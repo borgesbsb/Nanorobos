35a2f730b243d66a0dd8e70fb3e01b0a40ca5f6e
Benjamin  da Silva Borges <borges.bnjamin@gmail.com>
2017-12-03 23:42:42 -0400

    IMplementando

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Grafo;
@@ -37,15 +39,16 @@ int Subconjunto::getRank() {
 
 class Aresta {
 private:
-    int src,dst,peso;
+    int src,dst;
+    float peso;
 public:
     Aresta(){};
     void setSrc(int);
     int getSrc();
     void setDst(int);
     int getDst();
-    void setPeso(int);
-    int getPeso();
+    void setPeso(float);
+    float getPeso();
 };
 
 void Aresta::setSrc(int src) {
@@ -65,11 +68,11 @@ int Aresta::getDst() {
 }
 
 
-void Aresta::setPeso(int peso) {
+void Aresta::setPeso(float peso) {
     this->peso = peso;
 }
 
-int Aresta::getPeso() {
+float Aresta::getPeso() {
     return this->peso;
 }
 
@@ -103,40 +106,53 @@ T Vertice<T>::getItem() {
 
 class Grafo {
 private:
-    int **matriz;
-    Aresta *arestas;
+    float **matriz;
+    Aresta **arestas;
     int  ordem;
     int  tamanho;
     int  contadorAresta;
 public:
     Grafo(int, int);
-    Aresta* getAresta();
+    Aresta** getAresta();
     int getOrdem();
     int getTamanho();
-    void insertEdge(Vertice<int>*,Vertice<int>*,int);
+    void insertEdge(int,int,float);
+    float **getMatriz();
+
 };
 
 
 Grafo::Grafo(int ordem,int tamanho) {
     this->tamanho = tamanho;
     this->ordem = ordem;
-    this->matriz = new int*[this->ordem+1];
-    for (int i= 0; i<this->ordem+1;i++) {
-        this->matriz[i] = new int[this->ordem+1];
+    this->matriz = new float*[this->ordem];
+    for (int i= 0; i<this->ordem;i++) {
+        this->matriz[i] = new float[this->ordem];
     }
 
-    this->arestas = new Aresta[this->tamanho];
+    for (int i =0; i<this->ordem ;  i++) {
+        for (int j = 0;j<this->ordem;j++) {
+            this->matriz[i][j] = 0;
+        }
+    }
+
+    this->arestas = new Aresta*[this->tamanho];
+
+    for (int i=0; i<tamanho;i++) {
+        this->arestas[i] = new Aresta();
+    }
+
+    this->contadorAresta = 0;
 
 }
 
 
-void Grafo::insertEdge(Vertice<int> *u, Vertice<int> *v, int peso) {
-    this->matriz[u->getItem()][v->getItem()] = peso;
-    this->matriz[v->getItem()][u->getItem()] = peso;
-
-    this->arestas[contadorAresta].setSrc(u->getItem());
-    this->arestas[contadorAresta].setDst(v->getItem());
-    this->arestas[contadorAresta].setPeso(peso);
+void Grafo::insertEdge(int u , int v, float peso) {
+    this->matriz[u][v] = peso;
+    this->matriz[v][u] = peso;
+    this->arestas[contadorAresta]->setSrc(u);
+    this->arestas[contadorAresta]->setDst(v);
+    this->arestas[contadorAresta]->setPeso(peso);
     this->contadorAresta++;
 
 }
@@ -149,88 +165,197 @@ int Grafo::getTamanho() {
     return this->tamanho;
 }
 
+float **Grafo::getMatriz() {
+    return matriz;
+}
 
-Aresta* Grafo::getAresta() {
+
+Aresta** Grafo::getAresta() {
     return this->arestas;
 }
 
 
 class Heap {
 private:
-    Aresta *arestas;
+    Aresta **arestas;
     int tamanho;
-    int tamanhoHeap;
+    int heapsize;
+
+
 public:
-    Heap(Aresta*, int);
+    int tamanhoHeap;
+    Heap(Aresta**, int);
+    Heap(int);
     int pai(int);
     int esq(int);
     int dir(int);
-    void heapfica(int);
-    void constroiHeap();
-    Aresta* heapSort();
+    void maxheapify(int);
+    void minheapify(int);
+    void constroiMaxHeap();
+    void constroiMinHeap();
+    Aresta** heapSort();
+    Aresta* extraimin();
+    void decreasekey(int,Aresta*);
+    void insert(Aresta*);
+    Aresta **getAresta();
+
+
+
 
 };
 
-Heap::Heap(Aresta *arestas, int tamanho ) {
-    this->arestas =  new Aresta[tamanho];
-    this->arestas = arestas;
+Heap::Heap(int tamanho) {
+    this->tamanho = tamanho;
+    this->tamanhoHeap = 0;
+
+    this->arestas = new Aresta*[tamanho];
+    for (int i=0; i< tamanho;i++) {
+        this->arestas[i] = new Aresta();
+    }
+
+
+}
+
+Heap::Heap(Aresta **arestas, int tamanho ) {
+    this->arestas =  arestas;
     this->tamanho = tamanho;
 }
 
 int Heap::pai(int i) {
-    return i/2;
+    return  (i-1)/2;
 }
 
 int Heap::esq(int i ) {
-    return 2*i;
+    return 2*i + 1;
 }
 
 int Heap::dir(int i) {
-    return (2*i)+1;
+    return (2*i)+2;
 }
 
-void Heap::heapfica(int i ) {
+void Heap::minheapify(int i ) {
     int l = this->esq(i);
     int r = this->dir(i);
     int menor = i;
     Aresta *aux = new Aresta() ;
 
 
-    if (l < this->tamanhoHeap && this->arestas[l].getPeso() > this->arestas[menor].getPeso()) {
+    if (l < this->tamanhoHeap && this->arestas[l]->getPeso() <   this->arestas[menor]->getPeso()) {
         menor = l;
     }
 
-    if (r < this->tamanhoHeap  && this->arestas[r].getPeso() > this->arestas[menor].getPeso()  ) {
+    if (r < this->tamanhoHeap  && this->arestas[r]->getPeso() < this->arestas[menor]->getPeso()  ) {
         menor = r;
     }
 
     if (menor != i ) {
-        *aux = this->arestas[i];
+        aux = this->arestas[i];
         this->arestas[i] = this->arestas[menor];
-        this->arestas[menor] = *aux;
-        heapfica(menor);
+        this->arestas[menor] = aux;
+        minheapify(menor);
     }
 }
 
-void Heap::constroiHeap() {
+
+void Heap::maxheapify(int i ) {
+    int l = this->esq(i);
+    int r = this->dir(i);
+    int maior = i;
+    Aresta *aux = new Aresta() ;
+
+
+    if (l < this->tamanhoHeap && this->arestas[l]->getPeso() >   this->arestas[maior]->getPeso()) {
+        maior = l;
+    }
+
+    if (r < this->tamanhoHeap  && this->arestas[r]->getPeso() > this->arestas[maior]->getPeso()  ) {
+        maior = r;
+    }
+
+    if (maior != i ) {
+        aux = this->arestas[i];
+        this->arestas[i] = this->arestas[maior];
+        this->arestas[maior] = aux;
+        maxheapify(maior);
+    }
+}
+
+void Heap::constroiMaxHeap() {
     this->tamanhoHeap = this->tamanho;
     for (int i = (this->tamanho)/2 - 1; i >= 0  ; i--) {
-        this->heapfica(i);
+        this->maxheapify(i);
     }
 }
 
-Aresta* Heap::heapSort() {
-    this->constroiHeap();
+void Heap::constroiMinHeap() {
+    this->tamanhoHeap = this->tamanho;
+    for (int i = (this->tamanho)/2 - 1; i >= 0  ; i--) {
+        this->minheapify(i);
+    }
+}
+
+
+Aresta** Heap::heapSort() {
+    this->constroiMaxHeap();
     for (int i = this->tamanho-1; i >= 0; i--) {
-        Aresta *aux  =  new Aresta();
-        *aux = this->arestas[0];
+        Aresta *aux  =  this->arestas[0];
         this->arestas[0] = this->arestas[i];
-        this->arestas[i] = *aux;
+        this->arestas[i] = aux;
         this->tamanhoHeap--;
-        this->heapfica(0);
+        this->maxheapify(0);
     }
     return this->arestas;
 }
+
+Aresta* Heap::extraimin() {
+    Aresta *min;
+    if (this->tamanhoHeap < 0) {
+        cout <<  "underflow";
+    }
+
+    min = this->arestas[0];
+    this->arestas[0] = this->arestas[this->tamanhoHeap-1];
+    this->tamanhoHeap--;
+    this->tamanho--;
+    this->minheapify(0);
+    return min;
+}
+
+void Heap::decreasekey(int i , Aresta *key) {
+
+    if (key->getPeso() < this->arestas[i]->getPeso()) {
+        this->arestas[i] = key;
+        while ((i > 0 && this->arestas[this->pai(i)]->getPeso() > this->arestas[i]->getPeso())) {
+            Aresta *aux = this->arestas[i];
+            this->arestas[i] = this->arestas[this->pai(i)];
+            this->arestas[this->pai(i)] = aux;
+            i = this->pai(i);
+        }
+    }
+}
+
+
+void Heap::insert(Aresta *key) {
+
+    this->tamanhoHeap++;
+    int i = tamanhoHeap -1;
+    this->arestas[i] = key;
+
+    while (i != 0 && this->arestas[this->pai(i)]->getPeso() > this->arestas[i]->getPeso())
+    {
+        Aresta *aux = this->arestas[i];
+        this->arestas[i] = this->arestas[this->pai(i)];
+        this->arestas[this->pai(i)] = aux;
+        i = this->pai(i);
+    }
+
+
+}
+
+Aresta **Heap::getAresta() {
+    return this->arestas;
+}
+
 
 
 class Kruskal {
@@ -245,7 +370,7 @@ public:
     int findSet(int);
     void link(int, int);
     void uniondisjunt(int,int);
-    Aresta* mst();
+    float mst();
 };
 
 Kruskal::Kruskal(Grafo* grafo) {
@@ -289,9 +414,10 @@ void Kruskal::uniondisjunt(int vertice1 , int vertice2) {
     this->link( this->findSet(vertice1), this->findSet(vertice2));
 }
 
-Aresta* Kruskal::mst() {
-
+float Kruskal::mst() {
+    this->inc = 0;
     this->resultado = new Aresta[grafo->getOrdem()];
+    float soma = 0;
 
     for (int i = 0; i < grafo->getOrdem();i++) {
         this->subconjunto[i].setParent(i);
@@ -303,11 +429,11 @@ Aresta* Kruskal::mst() {
 
     for (int i = 0; i < grafo->getTamanho(); i++) {
 
-        int u = findSet(grafo->getAresta()[i].getSrc());
-        int v = findSet(grafo->getAresta()[i].getDst());
+        int u = findSet(grafo->getAresta()[i]->getSrc());
+        int v = findSet(grafo->getAresta()[i]->getDst());
 
         if ( u != v) {
-            this->resultado[inc] = grafo->getAresta()[i];
+            this->resultado[inc] = *grafo->getAresta()[i];
             this->inc++;
             this->uniondisjunt(u, v);
         }
@@ -315,52 +441,190 @@ Aresta* Kruskal::mst() {
     }
 
     for (int i = 0; i<inc;i++) {
-        cout << resultado[i].getSrc() <<" ---- "<<resultado[i].getDst() << " = "  << resultado[i].getPeso() << endl ;
+        soma = resultado[i].getPeso() + soma ;
     }
 
-    return resultado;
+    return soma;
 
 }
+
+class Dijkstra {
+private:
+
+    Heap *filaPrioridade;
+    vector<Aresta*> *vertices;
+    Grafo *grafo;
+    float **matriz;
+
+public:
+    Dijkstra(Grafo*);
+    void inicializa(int);
+    void relaxa(int,int,float);
+    Aresta* CaminhoMinimo(int);
+    vector<Aresta*> *getVertices();
+
+
+};
+
+Dijkstra::Dijkstra(Grafo *grafo) {
+
+    this->grafo = grafo;
+    this->filaPrioridade = new Heap(grafo->getOrdem());
+    this->vertices = new vector<Aresta*>();
+
+    this->matriz = new float*[this->grafo->getOrdem()];
+    for (int i= 0; i < this->grafo->getOrdem();i++) {
+        this->matriz[i] = new float[this->grafo->getOrdem()];
+    }
+    this->matriz = this->grafo->getMatriz();
+
+}
+
+void  Dijkstra::inicializa(int inicio) {
+
+    for (int i = 0; i < this->grafo->getOrdem();i++)   {
+        Aresta *vertice = new Aresta();
+        vertice->setDst(i);
+        vertice->setSrc(-1);
+        vertice->setPeso(9998);
+        this->vertices->push_back(vertice);
+        this->filaPrioridade->insert(vertice);
+
+    }
+
+    this->vertices->at(inicio)->setPeso(0);
+    this->filaPrioridade->constroiMinHeap();
+
+
+
+
+}
+
+void Dijkstra::relaxa(int u , int v , float w ) {
+
+    if (this->vertices->at(v)->getPeso() > this->vertices->at(u)->getPeso() + w  ) {
+        this->vertices->at(v)->setPeso( this->vertices->at(u)->getPeso() + w );
+        this->vertices->at(v)->setSrc(u);
+        this->filaPrioridade->constroiMinHeap();
+
+
+    }
+
+
+}
+
+
+Aresta* Dijkstra::CaminhoMinimo(int inicio) {
+
+    this->inicializa(inicio);
+
+    for (int i = 0; i < this->grafo->getOrdem(); i++) {
+
+        Aresta *u  = this->filaPrioridade->extraimin();
+
+
+        for (int j = 0 ; j < grafo->getOrdem();j++) {
+
+            if  (this->matriz[u->getDst()][j] != 0) {
+                this->relaxa(u->getDst(), j , matriz[u->getDst()][j]);
+
+            }
+
+        }
+
+    }
+
+}
+
+vector<Aresta*> *Dijkstra::getVertices() {
+    return this->vertices;
+}
+
 
 
 
 int main() {
 
-    int ordem = 9;
-    int tamanho = 14;
+    int ordem;
+    int tamanho;
+    int src;
+    int dst;
+    float peso;
+    int entrada;
+    int saida;
+    int qtdNeurDoentes;
+    int neuronioDoente;
+    bool *blocodoente;
+    float soma = 0;
 
-    Vertice<int> *vertice0 = new Vertice<int>(0);
-    Vertice<int> *vertice1 = new Vertice<int>(1);
-    Vertice<int> *vertice2 = new Vertice<int>(2);
-    Vertice<int> *vertice3 = new Vertice<int>(3);
-    Vertice<int> *vertice4 = new Vertice<int>(4);
-    Vertice<int> *vertice5 = new Vertice<int>(5);
-    Vertice<int> *vertice6 = new Vertice<int>(6);
-    Vertice<int> *vertice7 = new Vertice<int>(7);
-    Vertice<int> *vertice8 = new Vertice<int>(8);
-
-    Grafo *grafo = new Grafo(ordem,tamanho);
-
-    grafo->insertEdge(vertice7,vertice6,1);
-    grafo->insertEdge(vertice8,vertice6,2);
-    grafo->insertEdge(vertice6,vertice5,2);
-    grafo->insertEdge(vertice0,vertice1,4);
-    grafo->insertEdge(vertice2,vertice5,4);
-    grafo->insertEdge(vertice8,vertice6,6);
-    grafo->insertEdge(vertice2,vertice3,7);
-    grafo->insertEdge(vertice7,vertice8,7);
-    grafo->insertEdge(vertice0,vertice7,8);
-    grafo->insertEdge(vertice1,vertice2,8);
-    grafo->insertEdge(vertice3,vertice4,9);
-    grafo->insertEdge(vertice5,vertice4,10);
-    grafo->insertEdge(vertice1,vertice7,11);
-    grafo->insertEdge(vertice3,vertice5,14);
+    cin >> ordem;
+    cin >> tamanho;
 
 
-    Kruskal kruscal(grafo);
 
-    kruscal.mst();
+    Grafo *cerebro = new Grafo(ordem,tamanho);
+    Grafo **bloco = new Grafo*[ordem];
+    blocodoente = new bool[ordem];
 
+
+    //pegando as arestas
+    for (int i = 0; i < cerebro->getTamanho(); i++) {
+        cin >> src;
+        cin >> dst;
+        cin >> peso;
+        cerebro->insertEdge(src-1,dst-1,peso);
+    }
+
+        cin >> entrada;
+        cin >> saida;
+
+
+        //loop para pegar os blocos
+        for (int i = 0; i < cerebro->getOrdem(); i++) {
+            cin >> ordem;
+            cin >> tamanho;
+            cin >> qtdNeurDoentes;
+
+            for (int j = 0; j < qtdNeurDoentes;j++) {
+                cin >> neuronioDoente;
+            }
+
+            if (qtdNeurDoentes != 0) {
+                blocodoente[i] = true;
+            }else {
+                blocodoente[i] = false;
+            }
+
+            bloco[i] = new Grafo(ordem,tamanho);
+
+            //loop para pegar source e destino de cada bloco
+            for (int j = 0;j < tamanho; j++ ) {
+                cin >> src;
+                cin >> dst;
+                cin >> peso;
+                bloco[i]->insertEdge(src-1,dst-1,peso);
+            }
+        }
+
+    Dijkstra dijkstra(cerebro);
+    dijkstra.CaminhoMinimo(entrada-1);
+    vector<Aresta*> *navegar = dijkstra.getVertices();
+    int numeroBloco = navegar->at(saida-1)->getSrc();
+
+    if (blocodoente[saida-1]) {
+        Kruskal caminhoMinimoEntrada(bloco[saida - 1]);
+        soma = caminhoMinimoEntrada.mst() + soma;
+    }
+    while (numeroBloco != -1) {
+        if (blocodoente[numeroBloco]) {
+            Kruskal caminhoMinimo(bloco[numeroBloco]);
+            soma = caminhoMinimo.mst() + soma;
+        }
+            numeroBloco = navegar->at(numeroBloco)->getSrc();
+
+    }
+
+    cout << soma;
 
 
     return 0;
